489a0ece1ad0d159468854986e0bdae3e1c08d9a
pirana3 <pirana356@gmail.com>
2026-02-09 04:23:17 -0600

    before I sleeep make sure this is saved change the way how it works a bit

--- invoiceApp/app/(tabs)/profile/_layout.tsx
+++ invoiceApp/app/(tabs)/profile/_layout.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Pressable, useWindowDimensions } from 'react-native';
+import { TabView,  SceneMap} from 'react-native-tab-view';
+import {useState, useEffect} from 'react';
+import { Slot, usePathname, router } from 'expo-router';
 import React from 'react'
 
-const _layout = () => {
-  return (
-    <View>
-      <Text>_layout</Text>
-    </View>
-  )
-}
+const routes = [
+  {key: 'buisness', title: 'Buisness', path: '/profile/business'},
+  {key: 'employees', title: 'Employees', path: '/profile/employees'}
+];
 
-export default _layout
+
+export default function profileTabs () {
+  const layout = useWindowDimensions();
+
+  
+}
 
 const styles = StyleSheet.create({})
