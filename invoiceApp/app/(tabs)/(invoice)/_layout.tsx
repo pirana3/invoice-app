bc7e6649bd3489144c85ea9846d5b25b12d2db8b
pirana3 <pirana356@gmail.com>
2026-02-12 02:50:21 -0600

    adding changes to add the tabs folder for invocie

---
+++ invoiceApp/app/(tabs)/(invoice)/_layout.tsx
@@ -0,0 +1,20 @@
+import { Background } from "@react-navigation/elements";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { withLayoutContext } from "expo-router";
+
+const Tab = createMaterialTopTabNavigator()
+const TopTabs = withLayoutContext(Tab.Navigator);
+
+export default function ProfileLayout() {
+  return (
+    <TopTabs
+    screenOptions={{
+      tabBarIndicatorStyle: {backgroundColor: '#000'},
+      tabBarStyle: { backgroundColor: '#fff'},
+    }}
+    >
+      <TopTabs.Screen name='index' options={{ title: 'Invoice'}}/>
+
+    </TopTabs>
+  )
+}
