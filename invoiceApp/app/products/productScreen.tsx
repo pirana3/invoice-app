3b652335ca1258894b46c6fbf4fedac998eadaa6
pirana3 <pirana356@gmail.com>
2026-02-27 16:21:59 -0600

    product input fields created

--- invoiceApp/app/products/productScreen.tsx
+++ invoiceApp/app/products/productScreen.tsx
@@ -2,10 +2,27 @@ import { Text, View, TextInput } from 'react-native'
 import React from 'react'
 
 const productScreen = () => {
+    const[text, onChangeText] = React.useState('')
+    const [number, onChangeNumber] = React.useState('')
   return (
     <View>
       <TextInput
-      
+      onChangeText = {onChangeText}
+      value = {text}
+      placeholder='Enter the product name here.'
+      />
+
+      <TextInput
+      onChangeText ={onChangeText}
+      value = {text}
+      placeholder='Enter product description.'
+      />
+
+      <TextInput
+      onChangeText = {onChangeNumber}
+      value = {number}
+      placeholder= "Enter the product price here. $"
+      keyboardType="numeric"
       />
     </View>
   )
