cf9104f249fa66947fa62a787160009dcb753e03
pirana3 <pirana356@gmail.com>
2026-03-13 12:00:00 -0500

    start of the search componenet

---
+++ invoiceApp/components/EmployeeFilter.tsx
@@ -0,0 +1,14 @@
+import { Text, View, TouchableOpacity } from 'react-native';
+import {router, useLocalSearchParams} from "expo-router";
+import React, {useState} from 'react'
+
+const EmployeeFilter = () => {
+  return (
+    <View>
+      <Text>EmployeeFilter</Text>
+    </View>
+  )
+}
+
+export default EmployeeFilter
+
---
+++ invoiceApp/components/EmployeesSearchBar.tsx
@@ -0,0 +1,35 @@
+import { Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { useDebouncedCallBack} from "use-debounce";
+import { icons }from '@/constants/icons';
+import { useLocalSearchParams, router, usePathname} from 'expo-router';
+
+const EmployeesSearchBar = () => {
+    const path = usePathname();
+    const params = useLocalSearchParams<{ query?: string}>();
+    const [search, setSearch] = useState(params.query);
+
+    const debouncedSearch = useDebouncedCallback((text: string) => {
+        router.setParams({ query: text});
+    }, 500);
+
+    const handleSearch = (text: string) => {
+        setSearch(text);
+        debouncedSearch(text);
+    }
+
+  return (
+    <View className="flex flex-row items-center justify">
+        <View>
+            <Image/>
+            <TextInput 
+            />
+        </View>
+        <TouchableOpacity>
+            <Image/>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+export default EmployeesSearchBar
