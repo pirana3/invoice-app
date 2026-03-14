ea999a94383d6d5a88b28c055445ac1c68f36970
pirana3 <pirana356@gmail.com>
2026-03-14 04:58:05 -0500

    did some things for employee filter

--- invoiceApp/components/EmployeeFilter.tsx
+++ invoiceApp/components/EmployeeFilter.tsx
@@ -1,8 +1,22 @@
 import { Text, View, TouchableOpacity } from 'react-native';
 import {router, useLocalSearchParams} from "expo-router";
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { eCategories } from '@/constants/data';
 
 const EmployeeFilter = () => {
+  const paras = useLocalSearchParams<{ filter?: string}>();
+  const [selectedCategory, setSeletedCategory] = useState(paras.filter || "All");
+
+  const handleCategoryPress = (category: string) => {
+    if (selectedCategory === category) {
+        setSelectedCategory("");
+        router.setParams({ filter: ""});
+        return;
+    }
+
+    setSelectedCategory(category);
+    router.setParams({fileter: category});;
+  };
   return (
     <View>
       <Text>EmployeeFilter</Text>
---
+++ invoiceApp/constants/data.ts
@@ -0,0 +1,3 @@
+export const eCategories = [
+    { title: 'All', value: 'All' },
+]
