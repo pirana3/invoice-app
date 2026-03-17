14747f1a454f1362fed87e36eee600cf08d10f6a
pirana3 <pirana356@gmail.com>
2026-03-17 03:31:31 -0500

    imported icons for basic ui and set it up in the icon file to be easily exported

--- invoiceApp/constants/icons.ts
+++ invoiceApp/constants/icons.ts
@@ -1,7 +1,28 @@
 import add from "../assets/icon/add.png";
 import search from "../assets/icon/search.png";
+import x from "../assets/icon/x.png";
+import star from "../assets/icon/star.png";
+import emptystar from "../assets/icon/emptystar.png";
+import halfstar from "../assets/icon/halfstar.png";
+import filter from "../assets/icon/filter.png";
+import edit from "../assets/icon/edit.png";
+import settings from "../assets/icon/settings.png";
+import dropdownarrow from "../assets/icon/dropdownarrow.png";
+import backarrow from "../assets/icon/backarrow.png";
+import forwardarrow from "../assets/icon/forwardarrow.png";
+
 
 export const icons = {
   add,
-  search
+  search,
+  x,
+  star,
+  emptystar,
+  halfstar,
+  filter,
+  edit,
+  settings,
+  dropdownarrow,
+  backarrow,
+  forwardarrow
 };
