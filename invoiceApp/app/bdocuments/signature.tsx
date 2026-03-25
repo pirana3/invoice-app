d5c28cb3d77cfd25157e05e5742f20dd6eb6f72b
pirana3 <pirana356@gmail.com>
2026-03-25 01:08:59 -0500

    created the signatue modal and imported pdff and util

---
+++ invoiceApp/app/bdocuments/signature.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import Pdf from 'react-native-pdf';
+import React from 'react'
+
+const signature = () => {
+  return (
+    <View>
+      <Text>signature</Text>
+    </View>
+  )
+}
+
+export default signature
