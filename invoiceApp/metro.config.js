946e11df454c98e87c5d3d1c85ce5fbb902a5136
pirana3 <pirana356@gmail.com>
2026-05-04 17:08:01 -0500

    added the icon and made some bug fixes

--- invoiceApp/metro.config.js
+++ invoiceApp/metro.config.js
@@ -1,6 +1,16 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './app/global.css' })
+const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
+
+// EAS/Xcode can execute Metro from ios/, but NativeWind's Expo integration
+// resolves config via process.cwd(). Ensure cwd is always app root.
+if (process.cwd() !== __dirname) {
+  process.chdir(__dirname);
+}
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, {
+  input: path.resolve(__dirname, "app/global.css"),
+  configPath: path.resolve(__dirname, "tailwind.config.js"),
+});
